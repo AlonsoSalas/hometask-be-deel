0f64681518fd3f722793067aae6a9a84730c43ce
Alonso <alosalasv@gmail.com>
2024-01-19 06:10:05 -0300

    feat(backend-test): using Authorization error in middleware

--- src/middlewares/getProfile.js
+++ src/middlewares/getProfile.js
@@ -1,10 +1,11 @@
+const { AuthorizationError } = require("../errors");
 const { Profile } = require("../models");
 
 const getProfile = async (req, res, next) => {
   const profile = await Profile.findOne({
     where: { id: req.get("profile_id") || 0 },
   });
-  if (!profile) return res.status(401).end();
+  if (!profile) next(new AuthorizationError());
   req.profile = profile;
   next();
 };
